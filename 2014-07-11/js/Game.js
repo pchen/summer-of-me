9397883dc9b9031423b592651abab8f7f3a1a55c
Pearl Chen <pearl@karma-laboratory.com>
2014-07-13 00:23:03 -0400

    Day 22 update

--- 2014-07-11/js/Game.js
+++ 2014-07-11/js/Game.js
@@ -2,35 +2,27 @@
  * Creates the Game object which will hold all objects and data for
  * the game.
  */
-var Game = function() {
+(function (window) {
+  'use strict';
 
-  var background;
-
-  // Initialize objects to contain their context and canvas
-  // information
-  function init(imageRepository, canvas, ctx) {
+  function Game(imageRepository, bgCanvas) {
 
     // Initialize the background object
-    Background.prototype.context = ctx;
-    Background.prototype.canvasWidth = canvas.width;
-    Background.prototype.canvasHeight = canvas.height;
-    background = new Background(imageRepository.background);
-    background.init(0,0); // Set draw point to 0,0
+    var background = new app.Background(imageRepository.background, bgCanvas);
+
+    // Continuously redraw the game
+    function animate() {
+      requestAnimationFrame(animate.bind(this));
+      background.draw();
+    };
 
     // start the animation loop
     animate();
     
   };
 
-  // Continuously redraw the game
-  function animate() {
-    requestAnimationFrame(animate.bind(this));
-    background.draw();
-  };
+  // Export to window
+  window.app = window.app || {};
+  window.app.Game = Game;
 
-  // public functions
-  return {
-    init: init
-  };
-  
-};
+})(window);
